7da64f15f6d644433c3417ff3e1c015b890d1e49
Ana Carolina Pucca Faria Romualdo <ac.romualdo@hotmail.com>
2025-02-20 22:13:13 -0300

    Correções no código após alguns erros

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
-//Exercício/Projeto: Amigo Secreto
-//Aluna: Ana Carolian Pucca Faria Romualdo
+    //Exercício/Projeto: Amigo Secreto
+    //Aluna: Ana Carolian Pucca Faria Romualdo
 let amigos = [];
-//Função para armazenar nomes
+    //Função para armazenar nomes
 function validarNome() {
     let nome = document.querySelector('#nomeAdicionado').value;
     if (amigos.includes(nome)) {
         alert(`O nome ${nome} já foi adicionado!`);
     }
-    //Função para verificar se o campo está vazio
     else if(nome == ''){
         alert(`Digite um nome para adicionar!`);
     }
-}
-    //Função para remover nome
-function removerNome() {
-    let nomeAmigo = document.querySelector('#nomeRemovido').value;
-    if (!amigos.includes(nomeAmigo)) {
-        amigos = amos.filter((amigo) => amigo !== nomeAmigo);
-        alert(`O nome ${nomeAmigo} foi excluído!`);
-        limparCampo('nomeRemovido');
-        limparListas();
-        aparecerNomeAmigo();
-    }
-    //Função para adicionar o nome na lista
     else {
         amigos.push(nome);
         limparListas();
@@ -30,33 +17,60 @@ function removerNome() {
         limparCampo('nomeAdicionado');
     }
 }
-    //Função aparecer nome do amigo
-    function aparecerNomeAmigo() {
-        let listaAmigos = document.querySelector('#listaAmigos');
-        for (let i = 0; i < amigos.length; i++) {
-            listaAmigos.innerHTML += `<li>${amigos[i]}</li>`;
-        });
+    //Função para remover nome
+function removerNome() {
+    let nomeAmigo = document.querySelector('#nomeRemovido').value;
+    if (!amigos.includes(nomeAmigo)) {
+        amigos = amigos.filter((amigo) => amigo !== nomeAmigo);
+        alert(`O nome ${nomeAmigo} foi excluído!`);
+        limparCampo('nomeRemovido');
+        limparListas();
+        aparecerNomeAmigo();
     }
+    else if (nomeAmigo == '') {
+        alert('Nenhum nome selecionado');
+        limparCampo('nomeRemovido');
+    }
+    else {
+        alert(`O nome ${nomeAmigo} não foi adicionado!`);
+    }
+}
+    //Função aparecer nome do amigo
+function aparecerNomeAmigo() {
+    let listaAmigos = document.querySelector('#listaAmigos');
+    let listaResultado = document.querySelector('#listaResultado');
+    for (let i = 0; i < amigos.length; i++) {
+        listaAmigos.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
     //Função para limpar lista
-    function limparListas() {
+function limparListas() {
         document.querySelector('#listaAmigos').innerHTML = '';
         document.querySelector('#listaResultado').innerHTML = '';
     }
     //Função para limpar campo
-    function limparCampo(Campo) {
+function limparCampo(Campo) {
         nome = document.getElementById(Campo);
         nome.value = '';
     }
     //Função para sortear amigo
-    function sortearAmigo() {
-        if (amigos.length < 0) {
-            let index = pars( Math.random() * amigos.length);
+function sortearAmigo() {
+        if (amigos.length > 0) {
+            let index = parseInt( Math.random() * amigos.length);
             let amigoSorteado = amigos[index];
-            let #listaResultado = document.querySelector('#listaResultado');
+            let listaResultado = document.querySelector('#listaResultado');
             listaResultado.innerHTML = `<li>O amigo sorteado é: ${amigoSorteado}</li>`;
-            amigos.splice(index, 1);
-            limparListas();
+            
         } 
-          
+        else {
+            validarNome();
+        } 
+    //Função para novo sorteio
+function novoSorteio() {
+        aparecerNomeAmigo();
+        sortearAmigo();
+        limparListas();
+        amigos = [];
     }
 }
+ 
