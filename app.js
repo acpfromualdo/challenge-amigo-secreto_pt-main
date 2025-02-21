db7f0601f927f907a42a4673364a7f83f0d240f9
Ana Carolina Pucca Faria Romualdo <ac.romualdo@hotmail.com>
2025-02-20 21:08:06 -0300

    Funçoes para armazenar nome

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-//
+//Exercício/Projeto: Amigo Secreto
+//Aluna: Ana Carolian Pucca Faria Romualdo
 let amigos = [];
-
+//Função para armazenar nomes
 function validarNome() {
     let nome = document.querySelector('#nomeAdicionado').value;
     if (amigos.includes(nome)) {
         alert(`O nome ${nome} já foi adicionado!`);
     }
+    
 }
